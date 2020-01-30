477590438fcf96c0365241684fb252712aeee4de
victor tim <technicalpro@securemedia.ch>
2020-01-30 15:24:24 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,40 +19,34 @@ def assembled_matrix
 
 produce_isle = 
 [
-  CONVENTIONAL_PRODUCE = [
-  "Grapefruit",
-  "Pineapple",
-  "Oranges",
-  "Watermelon",
-  "Eggplant",
-],
-
-  ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus",
-]
+  CONVENTIONAL_PRODUCE,
+  ORGANIC_PRODUCE
 ]
 end
-# Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
 
 def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
-  # uses the arrays of conventional and organic produce as before.
-  # However, this time, sort each internal array alphabetically by the first character
-end
-def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+ [
+  produce_isle =
+  CONVENTIONAL_PRODUCE.sort,
+  ORGANIC_PRODUCE.sort
+  ]
 end
 
-def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+def assembled_matrix
+  
+  ([CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE])
+  
+end
+
+def matrix_lookup
+  expect(matrix_lookup(assembled_matrix, 1, 1))
+  expect(matrix_lookup(sorted_matrix, 1, 1))
+end
+
+def matrix_update 
+ local_copy_of_matrix = assembled_matrix.dup
+      result = matrix_update(local_copy_of_matrix, 0, 2)
+      expect(result[0])
+      result = matrix_update
+      expect(result[0])
 end
